b2ecb92d7a2004c656a3fd190e8526fd34673c01
Yogesh Kumar <yogeshkumar.taikisha@gmail.com>
2025-12-05 22:48:08 +0530

    initial commit

---
+++ basic.js
@@ -0,0 +1,6 @@
+function greetWithName(name) {
+    return " Hello ".concat(name);
+    // return "Hello " + name
+}
+console.log(greetWithName('yogesh'));
+// console.log(greet('42'))
---
+++ basic.ts
@@ -0,0 +1,7 @@
+function greetWithName(name: string): string {
+    return ` Hello ${name}`
+    // return "Hello " + name
+}
+
+console.log(greetWithName('yogesh'))
+// console.log(greet('42'))
