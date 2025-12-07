54bfb3e69fddfe66ffd9ac9ced34f16917e14830
Yogesh Kumar <yogeshkumar.taikisha@gmail.com>
2025-12-07 21:41:06 +0530

    type inference and type annotations in ts

---
+++ src/typeInts.ts
@@ -0,0 +1,17 @@
+let drink = "chai";
+// drink = 0;
+
+let cups = Math.random() > 0.5 ? 10 : 5;
+
+let cupsCalc = Math.random() > 0.5 ? 10 : "5";
+
+let channelName = "chai aur code";
+// channelName = 1221232;
+
+// type annotation
+let chaiFlavour: string = "masala chai";
+chaiFlavour = "ginger tea";
+// chaiFlavour = 2;
+
+let chaiOrder: number;
+let chaiOrder1: boolean;
